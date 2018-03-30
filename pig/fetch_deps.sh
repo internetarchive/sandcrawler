b67d08495cf8caa3fa52cf8f8f80389250685218
Bryan Newbold <bnewbold@archive.org>
2018-03-29 23:25:34 -0700

    basically working pig test

--- pig/fetch_deps.sh
+++ pig/fetch_deps.sh
@@ -2,18 +2,18 @@
 
 set -euo pipefail
 
-# If you change this, also update tests/pighelper.py
-PIG_VERSION="0.12.0-cdh5.0.1"
+#PIG_VERSION="0.12.0-cdh5.2.0"
+# Using more recent version to work around snappy classpath problem
+PIG_VERSION="0.17.0"
+JAVA_HOME=$(readlink -f /usr/bin/java | sed "s:bin/java::")
 
 mkdir -p deps/
 cd deps/
-wget -c https://archive.cloudera.com/cdh5/cdh/5/pig-${PIG_VERSION}.tar.gz
+
+# Fetch Pig
+#wget -c https://archive.cloudera.com/cdh5/cdh/5/pig-${PIG_VERSION}.tar.gz
+wget -c http://mirror.metrocast.net/apache/pig/pig-${PIG_VERSION}/pig-${PIG_VERSION}.tar.gz
 tar xvf pig-${PIG_VERSION}.tar.gz
 ln -fs pig-${PIG_VERSION} pig
-cd pig
-ln -fs pig-${PIG_VERSION}.jar pig.jar
-cd ..
-
-JAVA_HOME=$(readlink -f /usr/bin/java | sed "s:bin/java::")
 ./pig/bin/pig -x local -version
 
--- pig/tests/pighelper.py
+++ pig/tests/pighelper.py
@@ -1,9 +1,17 @@
 """
 A helper class for locally testing Pig scripts.
 
+Include `PigTestHelper` and extend in your test classes, call `self.run_pig()`
+with your script and example input file, then look at the output (at returned
+path) to check for validity.
+
+TODO: squelch pig stdout going to console, presumably because of subprocess.run
+behavior
+
 author: Bryan Newbold <bnewbold@archive.org>
 """
 import os
+import shutil
 import tempfile
 import unittest
 import subprocess
@@ -16,30 +24,26 @@ class PigTestHelper(unittest.TestCase):
     def setUpClass(cls):
 
         cls._pigpath= "./deps/pig/bin/pig"
+        cls._classpath = "./deps/hadoop/share/hadoop/common/lib"
         cls._base = [cls._pigpath,
             '-x', 'local',
-            '-log4jconf', 'pig_log4j.properties',
-            '-stop_on_failure']
+            '-P', './tests/pig.properties']
 
         # Check that pig is functioning
         if subprocess.call(cls._base + ['-version']) != 0:
             raise unittest.SkipTest("Failed to find and run Pig")
 
-        # Classpath?
-        # os.path.join("pig-0.12.0-cdh5.0.1", "pig.jar"),
-        # os.path.join("pig-0.12.0-cdh5.0.1", "lib", "*"),
-        # "hadoop-2.3.0-cdh5.0.1"
-
     def setUp(self):
         self._tmpdir = tempfile.mkdtemp()
 
     def tearDown(self):
-        os.rmdir(self._tmpdir)
+        shutil.rmtree(self._tmpdir)
 
     def run_pig_raw(self, params):
         """Low-level variant with params appended directly. Returns
         CompletedProcess, raises an error if return value isn't succes"""
 
+        print("Running: {}".format(' '.join(self._base + params)))
         retval = subprocess.run(self._base + params,
             timeout=20.0,
             check=True)
@@ -67,3 +71,4 @@ class PigTestHelper(unittest.TestCase):
         self.run_pig_raw(params)
         return out_file
 
+    # TODO: helper to verify that output matches an expected file
