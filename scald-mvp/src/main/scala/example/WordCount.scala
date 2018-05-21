53c2b11d4d234d933742651051ef57ca3c145382
Bryan Newbold <bnewbold@archive.org>
2018-05-21 11:57:25 -0700

    remove main function; class name same as file

--- scald-mvp/src/main/scala/example/WordCount.scala
+++ scald-mvp/src/main/scala/example/WordCount.scala
@@ -3,18 +3,7 @@ package example
 
 import com.twitter.scalding._
 
-object WordCountJob {
-
-  def main(args: Array[String]) {
-    (new WordCountJob(Args(List("--local", "", "--input", "dummy.txt", "--output", "dummy-out.txt")))).run
-
-    import io.Source
-    for (line <- Source.fromFile("dummy-out.txt").getLines())
-      println(line)
-  }
-}
-
-class WordCountJob(args : Args) extends Job(args) {
+class WordCount(args : Args) extends Job(args) {
   TypedPipe.from(TextLine(args("input")))
     .flatMap { line => line.split("""\s+""") }
     .groupBy { word => word }
