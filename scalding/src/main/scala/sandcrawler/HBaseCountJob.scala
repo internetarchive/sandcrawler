8d8ef0d8d19350228ae4d58267b156519ad1fc35
Ellen Spertus <ellen.spertus@gmail.com>
2018-06-08 17:15:02 -0700

    Tests pass.

---
+++ scalding/src/main/scala/sandcrawler/HBaseCountJob.scala
@@ -0,0 +1,30 @@
+package sandcrawler
+
+import cascading.property.AppProps
+import cascading.tuple.Fields
+import com.twitter.scalding._
+import java.util.Properties
+import parallelai.spyglass.base.JobBase
+import parallelai.spyglass.hbase.{HBaseSource, HBasePipeConversions}
+import parallelai.spyglass.hbase.HBaseConstants.SourceMode
+
+class HBaseCountJob(args: Args, colSpec: String) extends JobBase(args) with HBasePipeConversions {
+  val output = args("output")
+  // TODO: Add error checking.
+  val Col: String = colSpec.split(":")(1)
+
+  HBaseCountJob.getHBaseSource(colSpec)
+    .read
+    .fromBytesWritable(Symbol(Col))
+    .debug
+    .groupBy(Col){group => group.size('count)}
+    .write(Tsv(output))
+}
+
+object HBaseCountJob {
+  def getHBaseSource(colSpec: String) = HBaseBuilder.build(
+    "wbgrp-journal-extract-0-qa",     // HBase Table Name
+    "mtrcs-zk1.us.archive.org:2181",  // HBase Zookeeper server (to get runtime config info; can be array?)
+    List(colSpec),
+    SourceMode.SCAN_ALL)
+}
--- scalding/src/main/scala/sandcrawler/HBaseMimeCountJob.scala
+++ scalding/src/main/scala/sandcrawler/HBaseMimeCountJob.scala
@@ -8,21 +8,5 @@ import parallelai.spyglass.base.JobBase
 import parallelai.spyglass.hbase.{HBaseSource, HBasePipeConversions}
 import parallelai.spyglass.hbase.HBaseConstants.SourceMode
 
-class HBaseMimeCountJob(args: Args) extends JobBase(args) with HBasePipeConversions {
-  val output = args("output")
+class HBaseMimeCountJob(args: Args) extends HBaseCountJob(args, "file:mime") {}
 
-  HBaseMimeCountJob.getHBaseSource
-    .read
-    .fromBytesWritable(List('mime))
-    .debug
-    .groupBy('mime){group => group.size('count)}
-    .write(Tsv(output))
-}
-
-object HBaseMimeCountJob {
-  def getHBaseSource = HBaseBuilder.build(
-    "wbgrp-journal-extract-0-qa",     // HBase Table Name
-    "mtrcs-zk1.us.archive.org:2181",  // HBase Zookeeper server (to get runtime config info; can be array?)
-    List("file:mime"),
-    SourceMode.SCAN_ALL)
-}
