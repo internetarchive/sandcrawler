78f6f9bb555390050ecbf758f1f4d36fafa2a099
Ellen Spertus <ellen.spertus@gmail.com>
2018-06-09 10:34:07 -0700

    Cleaned up imports in HBaseMimeCountJob.scala.

--- scalding/src/main/scala/sandcrawler/HBaseMimeCountJob.scala
+++ scalding/src/main/scala/sandcrawler/HBaseMimeCountJob.scala
@@ -1,12 +1,6 @@
 package sandcrawler
 
-import cascading.property.AppProps
-import cascading.tuple.Fields
-import com.twitter.scalding._
-import java.util.Properties
-import parallelai.spyglass.base.JobBase
-import parallelai.spyglass.hbase.{HBaseSource, HBasePipeConversions}
-import parallelai.spyglass.hbase.HBaseConstants.SourceMode
+import com.twitter.scalding.Args
 
 class HBaseMimeCountJob(args: Args) extends HBaseCountJob(args, "file:mime") {}
 
