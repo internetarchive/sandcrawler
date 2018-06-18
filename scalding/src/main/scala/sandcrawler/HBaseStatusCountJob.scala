309f1fcb9dfe6968e7b1a0640280adf1b6c1ffc2
Ellen Spertus <ellen.spertus@gmail.com>
2018-06-18 11:28:22 -0700

    Changed grobid0:status to grobid0:status_code, per issue 1.

--- scalding/src/main/scala/sandcrawler/HBaseStatusCountJob.scala
+++ scalding/src/main/scala/sandcrawler/HBaseStatusCountJob.scala
@@ -2,4 +2,4 @@ package sandcrawler
 
 import com.twitter.scalding.Args
 
-class HBaseStatusCountJob(args: Args) extends HBaseCountJob(args, "grobid0:status")
+class HBaseStatusCountJob(args: Args) extends HBaseCountJob(args, "grobid0:status_code")
