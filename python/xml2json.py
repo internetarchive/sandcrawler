55957b81de5e438f2a245177d624040e74e721b3
Bryan Newbold <bnewbold@archive.org>
2019-09-25 17:58:21 -0700

    get rid of old xml2json

--- python/xml2json.py
+++
@@ -1,7 +0,0 @@
-
-import json
-import xmltodict
-
-with open('tests/files/23b29ea36382680716be08fc71aa81bd226e8a85.xml', 'rb') as f:
-    thing = xmltodict.parse(f, process_namespaces=False)
-    print(json.dumps(thing))
