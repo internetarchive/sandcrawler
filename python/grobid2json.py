5e169ce051884d2ebbdcbfde7cdc1d2b2efc4f74
Bryan Newbold <bnewbold@archive.org>
2019-11-13 20:54:30 -0800

    grobid2json: make lang detection flexible

--- python/grobid2json.py
+++ python/grobid2json.py
@@ -146,7 +146,8 @@ def teixml2json(content, encumbered=True):
 
     text = tei.find('.//{%s}text' % (ns))
     #print(text.attrib)
-    info['language_code'] = text.attrib['{%s}lang' % xml_ns]  # xml:lang
+    if text.attrib.get('{%s}lang' % xml_ns):
+        info['language_code'] = text.attrib['{%s}lang' % xml_ns]  # xml:lang
 
     if encumbered:
         el = tei.find('.//{%s}profileDesc/{%s}abstract' % (ns, ns))
