31f2545eb8af78cde9e4e4178489a8630aca0d09
Bryan Newbold <bnewbold@archive.org>
2020-03-05 00:40:00 -0800

    ia: catch wayback ChunkedEncodingError

--- python/sandcrawler/ia.py
+++ python/sandcrawler/ia.py
@@ -467,8 +467,11 @@ class WaybackClient:
             )
         except requests.exceptions.TooManyRedirects:
             raise WaybackError("redirect loop (wayback replay fetch)")
+        except requests.exceptions.ChunkedEncodingError:
+            raise WaybackError("ChunkedEncodingError (wayback replay fetch)")
         except UnicodeDecodeError:
             raise WaybackError("UnicodeDecodeError in replay request (can mean nasty redirect URL): {}".format(url))
+
         try:
             resp.raise_for_status()
         except Exception as e:
