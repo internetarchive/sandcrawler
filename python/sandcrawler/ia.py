cc26ea975e29eefa2e2d3565c55ba0ac0a491bb7
Bryan Newbold <bnewbold@archive.org>
2020-10-17 17:20:58 -0700

    ingest: experimentally reduce CDX API retry delay
    This code path is only working about 1/7 times in production. Going to
    try with a much shorter retry delay and see if we get no success with
    that. Considering also just disabling this attempt all together and
    relying on retries after hours/days.

--- python/sandcrawler/ia.py
+++ python/sandcrawler/ia.py
@@ -978,7 +978,7 @@ class SavePageNowClient:
                     url=spn_result.terminal_url,
                     datetime=spn_result.terminal_dt,
                     filter_status_code=filter_status_code,
-                    retry_sleep=10.0,
+                    retry_sleep=2.0,
                 )
             except KeyError as ke:
                 print("CDX KeyError: {}".format(ke), file=sys.stderr)
