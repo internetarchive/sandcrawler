55b4864c5f7ce024eec8ee0c92ff6edf27a172ef
Martin Czygan <martin.czygan@gmail.com>
2023-07-19 16:41:37 +0200

    sql: reduce random sample size
    taken from an uncommited change on `wbgrp-svc506` checkout

--- sql/reingest_old.sh
+++ sql/reingest_old.sh
@@ -13,7 +13,7 @@ sudo -u sandcrawler pipenv run \
 
 cat /srv/sandcrawler/tasks/reingest_old_current.json \
     | shuf \
-    | head -n1000000 \
+    | head -n250000 \
     | jq . -c \
     | kafkacat -P -b wbgrp-svc350.us.archive.org -t sandcrawler-prod.ingest-file-requests-daily -p -1
 
